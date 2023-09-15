ee97940b107c5d5381a0ad50eecbe5b08086f16e
wangkc <wkclz@qq.com>
2023-09-16 00:39:35 +0800

    移除Cookie

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 
-import Cookies from 'js-cookie'
-
 import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn' // 中文语言
 
@@ -77,7 +75,7 @@ directive(app)
 app.use(ElementPlus, {
   locale: locale,
   // 支持 large、default、small
-  size: Cookies.get('size') || 'default'
+  size: localStorage.getItem('size') || 'default'
 })
 
 app.mount('#app')

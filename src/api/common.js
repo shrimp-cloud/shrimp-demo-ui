320d4ffddfcc6ddeac4eff4c9c436bc1d617d61c
wangkc <wkclz@qq.com>
2023-09-18 00:29:16 +0800

    cms 同步

--- src/api/common.js
+++ src/api/common.js
@@ -3,3 +3,7 @@ import request from '@/utils/request'
 export function commonDictsList(params) {
   return request({url: '/sys/common/dicts/list', method: 'get', params})
 }
+
+export function imageLibraryPage(params) {
+  return request({ url: '/cms/image/library/page', method: 'get', params });
+}

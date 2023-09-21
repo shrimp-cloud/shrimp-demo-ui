542dd9b0cd16e53c90641ff26994f0028e21b264
wangkc <wkclz@qq.com>
2023-09-22 00:06:16 +0800

    图片 common 接口

--- src/api/common.js
+++ src/api/common.js
@@ -4,6 +4,6 @@ export function commonDictsList(params) {
   return request({url: '/sys/common/dicts/list', method: 'get', params})
 }
 
-export function imageLibraryPage(params) {
-  return request({ url: '/cms/image/library/page', method: 'get', params });
+export function commonImagePage(params) {
+  return request({ url: '/cms/common/image/page', method: 'get', params });
 }

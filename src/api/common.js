e60fb1eee65d2dd88b9eadaaee07207cc5788d24
wangkc <wkclz@qq.com>
2024-03-25 00:54:38 +0800

    图片选取组件更新

--- src/api/common.js
+++ src/api/common.js
@@ -4,6 +4,10 @@ export function commonDictsList(params) {
   return request({url: '/auth/common/dicts/list', method: 'get', params})
 }
 
+export function imageCategoryOptionsWithimgcount(params) {
+  return request({url: '/cms/image/category/options/withimgcount', method: 'get', params});
+}
+
 export function commonImagePage(params) {
   return request({ url: '/cms/common/image/page', method: 'get', params });
 }
--- src/components/aa/ShImage/index.vue
+++ src/components/aa/ShImage/index.vue
@@ -14,40 +14,62 @@
       </template>
     </el-image>
 
-    <el-dialog title="请选择图片" v-model="open" width="840px">
-      <el-form :model="queryParams" ref="queryRef" :inline="true" label-width="68px">
-        <el-form-item prop="imageType">
-          <el-radio-group v-model="queryParams.imageType" class="ml-4" @change="handleQuery">
-            <el-radio label="" size="large">全部</el-radio>
-            <el-radio v-for="dict in IMAGE_TYPE" :label="dict.value" size="large">{{dict.label}}</el-radio>
-          </el-radio-group>
-        </el-form-item>
-      </el-form>
-
-      <div v-if="!dataList || dataList.length ===0">
-        <el-empty description="无图片" />
-      </div>
-      <el-row v-if="dataList && dataList.length > 0" :gutter="10" class="mb8">
-        <el-col :span="4" v-for="item in dataList">
-          <img :src="item.imageUrl" :alt="item.imageName" style="width: 120px; height: 120px;" @click="click(item)"/>
-          <span>{{item.imageName}}</span>
+    <el-dialog title="请选择图片" v-model="open" width="1080px">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div style="height: 480px; overflow-y: auto; overflow-x: hidden;">
+            <el-tree
+                ref="treeRef"
+                class="filter-tree"
+                :data="categoryOptions"
+                :props="{label: 'categoryName', value: 'categoryCode', disabled: 'disabled'}"
+                :render-after-expand="false"
+                :expand-on-click-node="false"
+                node-key="categoryCode"
+                :check-strictly="true"
+                default-expand-all
+                @node-click="choseCategory"
+            >
+              <template #default="{ node, data }">
+                  <span class="custom-tree-node">
+                    <span>{{ data.categoryName }}</span>
+                    <span v-if="data.imageCount" style="padding-left: 6px;">
+                      <el-tag type="info">{{ data.imageCount }}</el-tag>
+                    </span>
+                  </span>
+              </template>
+            </el-tree>
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div style="height: 480px; overflow-y: auto; overflow-x: hidden;">
+            <div v-if="!dataList || dataList.length ===0">
+              <el-empty description="无图片" />
+            </div>
+            <el-row v-if="dataList && dataList.length > 0" :gutter="10" class="mb8">
+              <el-col :span="4" v-for="item in dataList">
+                <img :src="item.imageUrl" :alt="item.imageName" style="width: 120px; height: 120px;" @click="click(item)"/>
+                <div>{{item.imageName}}</div>
+              </el-col>
+            </el-row>
+            <pagination
+                v-show="total > 0"
+                :total="total"
+                :page-sizes="[18]"
+                v-model:page="queryParams.current"
+                v-model:limit="queryParams.size"
+                @pagination="getList"
+            />
+          </div>
         </el-col>
       </el-row>
-
-      <pagination
-          v-show="total > 0"
-          :total="total"
-          :sizes="[12,24,48,96]"
-          v-model:page="queryParams.current"
-          v-model:limit="queryParams.size"
-          @pagination="getList"
-      />
     </el-dialog>
   </div>
 </template>
 
 <script setup name="CommonImage">
 import { commonImagePage } from "@/api/common";
+import {imageCategoryOptionsWithimgcount} from "@/api/common";
 
 const props = defineProps({
   modelValue: String,
@@ -68,18 +90,18 @@ const props = defineProps({
 defineExpose({show})
 const emit = defineEmits(['update:modelValue', "change"]);
 const { proxy } = getCurrentInstance();
-const { IMAGE_TYPE } = proxy.useDict("IMAGE_TYPE");
 
 const realWidth = computed(() => typeof props.width == "string" ? props.width : `${props.width}px`);
 const realHeight = computed(() => typeof props.height == "string" ? props.height : `${props.height}px`);
 
+const categoryOptions = ref([]);
 const dataList = ref([]);
 const open = ref(false);
 const loading = ref(true);
 const total = ref(0);
 const queryParams = ref({
   current: 1,
-  size: 12,
+  size: 18,
   imageType: undefined,
 });
 const image = ref({
@@ -89,6 +111,26 @@ const image = ref({
 
 function show() {
   open.value = true;
+  handleQuery();
+}
+
+function init() {
+  getCategoryOptions();
+}
+
+function getCategoryOptions() {
+  imageCategoryOptionsWithimgcount().then(res => {
+    const options = [{categoryCode: '', categoryName: '全部'}];
+    for (const d of res.data) {
+      options.push(d);
+    }
+    categoryOptions.value = options;
+  });
+}
+
+function choseCategory(data, node, treeNode, event) {
+  queryParams.value.categoryCode = data.categoryCode;
+  queryParams.value.categoryName = data.categoryName;
   handleQuery();
 }
 
@@ -122,9 +164,10 @@ function click(val) {
   open.value = false;
 }
 
+init();
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .image-slot {
   display: flex;
   justify-content: center;

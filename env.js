2c002221f0844c3cc32bd008172eebfda7458261
wangkc <wkclz@qq.com>
2024-12-28 18:25:17 +0800

    代码排版

--- env.js
+++ env.js
@@ -2,27 +2,20 @@ const name = "示例系统";
 export const appCode = 'shrimp-demo';
 
 const configs = {
-  // "local": {"cas": "http://cas.uat.wkclz.com", "title": name + "【LOCAL】", "baseApi": "/api"}
+//"local": {"cas": "http://cas.uat.wkclz.com", "title": name + "【LOCAL】", "baseApi": "/api"},
   "local": {"cas": "http://cas.uat.wkclz.com", "title": name + "【LOCAL】", "baseApi": "https://api.uat.wkclz.com"},
-  "uat"  : {"cas": "http://cas.uat.wkclz.com", "title": name + "【UAT】", "baseApi": "https://api.uat.wkclz.com"},
-  "prod" : {"cas": "http://cas.wkclz.com",     "title": name,             "baseApi": "https://api.wkclz.com"}
+  "uat"  : {"cas": "http://cas.uat.wkclz.com", "title": name + "【UAT】",   "baseApi": "https://api.uat.wkclz.com"},
+  "prod" : {"cas": "http://cas.wkclz.com",     "title": name,               "baseApi": "https://api.wkclz.com"}
 };
 
 function getEnv() {
   let url = window.location.host;
   let env = 'prod';
-  if (url.indexOf('127.0.0.1') > -1 || url.indexOf('localhost') > -1) {
-    env = 'local';
-  }
-  if (url.indexOf('.dev.') > -1) {
-    env = 'dev';
-  }
-  if (url.indexOf('.sit.') > -1) {
-    env = 'sit';
-  }
-  if (url.indexOf('.uat.') > -1) {
-    env = 'uat';
-  }
+  if (url.indexOf('127.0.0.1') > -1) {env = 'local';}
+  if (url.indexOf('localhost') > -1) {env = 'local';}
+  if (url.indexOf('.dev.') > -1) {env = 'dev';}
+  if (url.indexOf('.sit.') > -1) {env = 'sit';}
+  if (url.indexOf('.uat.') > -1) {env = 'uat';}
   return env;
 }
 

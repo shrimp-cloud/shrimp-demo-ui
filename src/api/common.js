07941c5e198f4384897527bddcf35ed5359ef24e
wangkc <wkclz@qq.com>
2025-02-13 22:27:19 +0800

    增加表单接口

--- src/api/common.js
+++ src/api/common.js
@@ -4,6 +4,14 @@ export function commonDictsList(params) {
   return request({url: '/mdm/common/dicts/list', method: 'get', params})
 }
 
+export function commonFormList(params) {
+  return request({url: '/mdm/common/form/list', method: 'get', params})
+}
+
+export function commonFormDetail(params) {
+  return request({url: '/mdm/common/form/detail', method: 'get', params})
+}
+
 export function imageCategoryOptionsWithimgcount(params) {
   return request({url: '/cms/image/category/options/withimgcount', method: 'get', params});
 }

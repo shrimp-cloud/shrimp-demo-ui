be362fe786204b53a337221261daf11b9e95a972
wangkc <wkclz@qq.com>
2025-02-28 23:39:58 +0800

    commonUpload 放到公共接口中

--- src/api/common.js
+++ src/api/common.js
@@ -1,21 +1,28 @@
 import request from '@/utils/request'
 
+// 字典
 export function commonDictsList(params) {
   return request({url: '/mdm/common/dicts/list', method: 'get', params})
 }
 
+// 动态表单
 export function commonFormList(params) {
   return request({url: '/mdm/common/form/list', method: 'get', params})
 }
-
 export function commonFormDetail(params) {
   return request({url: '/mdm/common/form/detail', method: 'get', params})
 }
 
+
+// 文件上传
+export function commonUpload(data) {
+  return request({ url: '/mdm/common/upload', method: 'post', data });
+}
+
+// 图片选择
 export function imageCategoryOptionsWithimgcount(params) {
   return request({url: '/cms/image/category/options/withimgcount', method: 'get', params});
 }
-
 export function commonImagePage(params) {
   return request({ url: '/cms/common/image/page', method: 'get', params });
 }

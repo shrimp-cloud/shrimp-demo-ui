d0415549399dc1e419a543c79b53d2e4dbd89333
wangkc <wkclz@qq.com>
2025-05-09 22:43:56 +0800

    格式化

--- src/api/cas.js
+++ src/api/cas.js
@@ -3,27 +3,35 @@ import request from '@/utils/request'
 export function publicCaptchaPicture(params) {
     return request({url: '/cas/public/captcha/picture', method: 'get', params})
 }
+
 export function publicSsoLogin(data) {
     return request({url: '/cas/public/sso/login', method: 'post', data})
 }
+
 export function publicSsoLogout() {
     return request({url: '/cas/public/sso/logout', method: 'get'})
 }
+
 export function ssoBizLogin(data) {
     return request({url: '/cas/sso/biz/login', method: 'post', data})
 }
+
 export function publicSsoLoginUserCode() {
     return request({url: '/cas/public/sso/login/userCode', method: 'get'})
 }
+
 export function userResTree() {
     return request({url: '/cas/user/res/tree', method: 'get'})
 }
+
 export function userAppList() {
     return request({url: '/cas/user/app/list', method: 'get'})
 }
+
 export function userAppTenantList() {
     return request({url: '/cas/user/app/tenant/list', method: 'get'})
 }
+
 export function register() {
     return;
 }
